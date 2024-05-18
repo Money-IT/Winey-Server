b1406aa4b351db0acc1a0195174c2e55dd5c1f2a
Seonheui Jeon <88873302+funnysunny08@users.noreply.github.com>
2024-05-18 20:20:24 +0900

    [Test] 소셜 로그인 로그 (#250)

--- src/main/java/org/winey/server/service/auth/kakao/KakaoSignInService.java
+++ src/main/java/org/winey/server/service/auth/kakao/KakaoSignInService.java
@@ -3,6 +3,7 @@ package org.winey.server.service.auth.kakao;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonArray;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KakaoSignInService {
@@ -24,7 +26,9 @@ public class KakaoSignInService {
         headers.add("Authorization","Bearer "+ accessToken);
         HttpEntity<JsonArray> httpEntity = new HttpEntity<>(headers);
         ResponseEntity<Object> responseData;
+        log.info("before kakao post");
         responseData = restTemplate.postForEntity(KAKAO_URL,httpEntity,Object.class);
+        log.info("after kakao post");
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.convertValue(responseData.getBody(), Map.class).get("id").toString(); //소셜 id만 가져오는듯.
     }
